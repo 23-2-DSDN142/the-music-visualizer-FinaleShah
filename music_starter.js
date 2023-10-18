44ed9dc9b01b4b77fa6bbec2712ccadbf4fad12e
FinaleShah <shafinn3465@gmail.com>
2023-10-18 15:50:32 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,48 +1,83 @@
 
+
+let firstRun = true;
+let pearl = [];
+
+
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+  background(131, 109, 191);
+  
+if(firstRun){
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
+  rectMode(CENTER);
+  pearl.push (loadImage( 'pearl0.png'));
+  pearl.push(loadImage( 'pearl1.png'));
 
-   let ballSize = 40;
+ firstRun = false
 
-   fill(0);
-   ellipse(width/2,drum,ballSize);
- 
-
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
 }
+
+let pFrame = int(map(drum, 0, 100, 0, 3));
+
+let width = 100;
+
+console.log(pFrame);
+push();
+scale(0.3);
+image(pearl[pFrame], width/2, height/2);
+
+
+// fill(255);
+// ellipse(100,100,100,100);
+
+// let 
+
+  }
+
+
+
+
+// textFont('Helvetica'); // please use CSS safe fonts
+// rectMode(CENTER)
+// textSize(24);
+
+//  let bar_spacing = height / 10;
+//  let bar_height = width / 12;
+//  let bar_pos_x = width / 2;
+
+//  let ballSize = 40;
+
+//  fill(0);
+//  ellipse(width/2,drum,ballSize);
+
+
+//  // vocal bar is red
+//  fill(200, 0, 0);
+//  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+//  fill(0);
+//  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+
+//  // drum bar is green
+//  fill(0, 200, 0);
+//  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+//  fill(0);
+//  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+
+//  // bass bar is blue
+//  fill(50, 50, 240);
+//  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+//  fill(0);
+//  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+
+//  // other bar is white
+//  fill(200, 200, 200);
+//  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+//  fill(0);
+//  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+//  fill(255, 255, 0);
+
+//  // display "words"
+//  textAlign(CENTER);
+//  textSize(vocal);
+//  text(words, width/2, height/3);
